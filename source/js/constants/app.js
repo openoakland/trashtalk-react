a21650ec744a218c6d74d53b6ee1047f5efe5f2d
Thinh Ngo <thinh.ngo@gmail.com>
2018-05-10 12:06:32 -0700

    #21 Added rudimentary environmenty switching

--- source/js/constants/app.js
+++ source/js/constants/app.js
@@ -1,2 +1,10 @@
-// TODO: We'll have to fill this in dynamically based on env
-export const API_URL = 'http://localhost:8000';
+/*
+TODO: We'll want to set up a more sophisticated way of figuring out what the API_URL is. While this works for now,
+the limitations are:
+
+- It assumes there are only two environments
+- When developing the frontend, we assume there's a running Django instance at localhost:8000
+*/
+
+const IS_DEV = window.location.host.indexOf('localhost') === 0;
+export const API_URL = IS_DEV ? 'http://localhost:8000' : 'http://trashtalk.divethree.com';
